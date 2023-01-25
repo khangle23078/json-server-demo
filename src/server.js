cb2f20184ec5040e68f97dd2edfc2c97cc2567bf
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-01-25 14:56:40 +0700

    update commit

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth');
+const cors = require('cors');
+const dotenv = require('dotenv')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const PORT = 3001;
+dotenv.config()
 
 server.use(middlewares)
 server.use(router)
 server.use(auth)
+server.use(cors)
 server.use(jsonServer.bodyParser)
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
    console.log(`JSON Server is running in PORT ${PORT}`)
 })
